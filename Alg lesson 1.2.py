9f88a3f487e0180206423dc051f0df114eb7e426
Koral110 <49456546+Koral110@users.noreply.github.com>
2019-04-10 17:20:04 +0500

    Урок 1

---
+++ Alg lesson 1.2.py
@@ -0,0 +1,13 @@
+# Задание 2
+a = 5
+print("%d = %s" % (a, bin(a)))
+b = 6
+print("%d = %s" % (b, bin(b)))
+
+print("%d & %d = %d (%s)" % (a, b, a & b, bin(a & b)))
+print("%d | %d = %d (%s)" % (a, b, a | b, bin(a | b)))
+
+print("%d << 2 = %d (%s)" % (a, a << 2, bin(a << 2)))
+print("%d >> 2 = %d (%s)" % (a, a >> 2, bin(a >> 2)))
+print("%d << 2 = %d (%s)" % (b, b << 2, bin(b << 2)))
+print("%d >> 2 = %d (%s)" % (b, b >> 2, bin(b >> 2)))
